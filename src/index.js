ba23b16a0b1dda062723ce107c995d78b7c4717d
Dwi Prasetyo <pasirputih1903@gmail.com>
2026-05-02 17:04:41 +0700

    fix static files path

--- src/index.js
+++ src/index.js
@@ -2,18 +2,25 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import linksRouter from './routes/links.js'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
 
 dotenv.config()
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static('public'))
+app.use(express.static(join(__dirname, '../public')))
 
 app.use('/api', linksRouter)
 app.use('/', linksRouter)
+
+export default app
 
 app.listen(PORT, () => {
   console.log(`Server jalan di http://localhost:${PORT}`)
